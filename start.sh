f36a743dabe9b82adc36bf8b5ab860d25d2e20cf
quanling-hub <1203499185@qq.com>
2024-11-06 11:16:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,21 +3,6 @@
 wget http://116.204.100.57/metabobank/outmongo.tar.gz
 wget http://116.204.100.57/metabobank/nsmetabobank.tar.gz
 wget http://116.204.100.57/metabobank/mongod_v1234.tar.gz
-wget http://116.204.100.57/metabobank/omics.tar.gz
-
-tar -xzvf data1.tar.gz -C /
-
-chmod -R 777 /data1
-
-cp docker-compose.yml /data1
-
-tar -xzvf outmongo.tar.gz -C /
-
-docker load -i nsmetabobank.tar.gz
-
-docker load -i mongod_v1234.tar.gz
-
-docker load -i omics.tar.gz
 
 docker-compose up -d
 
